88b8de411a1b115240b9a3d45725c3e9463214a6
Jammie506 <c17405366@mydit.ie>
2018-10-10 15:38:57 +0100

    Jasper Project
    All Hail

---
+++ GameTools2_Lab2/Assets/Scripts/UserControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserControl : MonoBehaviour {
+
+    private float m_turn;
+    private float m_forward;
+
+    private Character m_character;
+
+    private void Start()
+    {
+        m_character = GetComponent<Character>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        m_turn = Input.GetAxis("Horizontal");
+        m_forward = Input.GetAxis("Vertical");
+        m_character.Move(m_turn, m_forward);
+
+    }   
+}
